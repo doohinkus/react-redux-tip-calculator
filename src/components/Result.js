486c42d65ee63b2a609c9571bc3f33119b368dd6
doohinkus <doohinkus@gmail.com>
2019-11-02 10:18:15 -0700

    update styles and add form

--- src/components/Result.js
+++ src/components/Result.js
@@ -23,52 +23,57 @@ function Result({
     state.bill)) / 
     parseInt(state.split))
     .toFixed(2);
+  const colorHightlight= {
+    color: "#FFFF66"
+  }
     return (
       <div>  
-      <h1>Your Amount: <br />${isNaN(result) || result}</h1>
-      <p>Bill:  
-        <Input 
-          title="Tab/Bill"
-          field="bill"
-          setError={setError}
-          setResult={setResult}
-          clearError={clearError}
-          action={setBill}
-          className={null}
-          step={.01}
-          min={.01}
-        />
-      </p>
-      <h3>{isError && error}</h3>
-       <p>
-        Split: 
-        <Input 
-          title="Split"
-          field="split"
-          setError={setError}
-          setResult={setResult}
-          clearError={clearError}
-          action={setSplit}
-          className="shorter"
-          step={1}
-          min={1}
-        />  person{state.split>1 && "s"}
-       </p>
-        <p>Tip Amount ( 
-          <Input 
-            title="Tip"
-            field="tip"
-            error={null}
-            isError={false}
-            action={setTip}
-            setError={setError}
-            setResult={setResult}
-            clearError={clearError}
-            className="short"
-            step={1}
-            min={0}
+      <h1>Your Amount: <br /><span style={colorHightlight}>${isNaN(result) || result}</span></h1>
+      <form onSubmit={(e) => e.preventDefault()}>
+          <p>Bill:  
+            <Input 
+              title="Tab/Bill"
+              field="bill"
+              setError={setError}
+              setResult={setResult}
+              clearError={clearError}
+              action={setBill}
+              className={null}
+              step={.01}
+              min={.01}
             />
-          %): ${((state.bill / state.split) * state.tip/100).toFixed(2)}</p>
+          </p>
+          <h3>{isError && error}</h3>
+          <p>
+            Split: 
+            <Input 
+              title="Split"
+              field="split"
+              setError={setError}
+              setResult={setResult}
+              clearError={clearError}
+              action={setSplit}
+              className="shorter"
+              step={1}
+              min={1}
+            />  person{state.split>1 && "s"}
+          </p>
+            <p>Tip Amount ( 
+              <Input 
+                title="Tip"
+                field="tip"
+                error={null}
+                isError={false}
+                action={setTip}
+                setError={setError}
+                setResult={setResult}
+                clearError={clearError}
+                className="short"
+                step={1}
+                min={0}
+                />
+              %): <br /><span style={colorHightlight}>${((state.bill / state.split) * state.tip/100).toFixed(2)}</span></p>
+      </form>
     
     </div>
   )
